e4d9d017e097a4a88c56f18610c04304de821f7d
Mario <Mario@DESKTOP-VNRJJNO>
2020-05-25 20:56:06 -0400

    Add project files.

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double centigrado;
+            double faradar;
+            string entrada;
+
+            Console.Write("\n\n\t\tDigite El Grado Celsius Para Convertilo Grados Fahrenheit:");
+            entrada = Console.ReadLine();
+            centigrado = Convert.ToDouble(entrada);
+            faradar = (1.8 * centigrado ) + 32;
+            Console.Write("\nEste Es El Grados Fahrenheit Otenido:" + faradar);
+        }
+    }
+}
